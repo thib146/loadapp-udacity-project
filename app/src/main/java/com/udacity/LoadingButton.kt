061a2af076b1f7808cd84c2104fc532b1b62abcb
thib146 <thibaut@jumboprivacy.com>
2023-08-28 11:47:24 +0200

    add first elements in main screen + download button default UI

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,6 +3,8 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 import kotlin.properties.Delegates
@@ -19,15 +21,34 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+    }
 
     init {
+        isClickable = true
+    }
 
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        return true
     }
 
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
+        paint.color = resources.getColor(R.color.colorPrimary, null)
+        canvas?.drawRect(width.toFloat(), height.toFloat(), 0f, 0f, paint)
+
+        paint.color = Color.WHITE
+        val textResource = resources.getString(R.string.button_download)
+        val textXPos = (width/2).toFloat()
+        val textYPos = (height/2).toFloat() - (paint.ascent()+paint.descent())/2
+        canvas?.drawText(textResource, textXPos, textYPos, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
